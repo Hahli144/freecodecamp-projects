015ec26bb60c984f9dd0dfd348ca0f47230b13bb
Salah Adawi <53702890+Hahli144@users.noreply.github.com>
2019-09-14 14:15:30 +0300

    Create roman-numeral-converter.js

---
+++ javascript-algorithms-and-data-structures/roman-numeral-converter.js
@@ -0,0 +1,18 @@
+function convertToRoman(num) {
+  var romanSym = ["I","X","C","M","V","L","D"];
+  var romanNum = [];
+  num = num.toString().split("").reverse();
+  for (var i=0;i<num.length;i++) {
+    if (num[i] < 4)
+      romanNum.push(romanSym[i].repeat(num[i]));
+    else if (num[i] == 4)
+      romanNum.push(romanSym[i] + romanSym[i + 4]);
+    else if (num[i] == 5)
+      romanNum.push(romanSym[i + 4]);
+    else if (num[i] < 9)
+      romanNum.push(romanSym[i + 4] + romanSym[i].repeat(num[i] - 5));
+    else if (num[i] == 9)
+      romanNum.push(romanSym[i] + romanSym[i + 1]);
+  }
+  return romanNum.reverse().join("");
+}
