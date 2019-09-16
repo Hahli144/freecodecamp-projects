99a08f4d2392dacd87041ddd9cb92d5f3f60cb4d
Salah Adawi <53702890+Hahli144@users.noreply.github.com>
2019-09-16 16:37:06 +0300

    Cleaned up code, shortened by 12 lines.

--- javascript-algorithms-and-data-structures/cash-register.js
+++ javascript-algorithms-and-data-structures/cash-register.js
@@ -1,11 +1,8 @@
 function checkCashRegister(price, cash, cid) {
-  cid.forEach(function (x) {
-    x[1] = Math.round(x[1] * 100);
-  });
+  cid.forEach(x => x[1] = Math.round(x[1] * 100));
   var after = cid.map(x => ([...x]));
   var amount = Math.round(cash * 100 - price * 100);
   var unit = [1, 5, 10, 25, 100, 500, 1000, 2000, 10000];
-  var statement = {status: "INSUFFICIENT_FUNDS", change: []}
   for (var i=8;i>=0;i--) {
       while (after[i][1] >= unit[i] && amount - unit[i] >= 0) {
         amount -= unit[i];
@@ -13,24 +10,15 @@ function checkCashRegister(price, cash, cid) {
       }
   }
   if (amount != 0)
-    return statement;
-  for (var j=0;j<cid.length;j++) {
+    return {status: "INSUFFICIENT_FUNDS", change: []};
+  for (var j=0;j<cid.length;j++)
     after[j][1] = cid[j][1] - after[j][1]
-  }
-  cid.forEach(function (x, k) {
+  cid.forEach((x, k) => {
     x[1] /= 100
     after[k][1] /= 100
   });
-  if (cid.every(function (x, k) {
-    return x[1] == after[k][1]
-  })) {
-    statement.status = "CLOSED";
-    statement.change = cid;
-  }
-  else {
-    statement.status = "OPEN"
-    statement.change = after.reverse().filter(function (x) {
-    return x[1] > 0
-  })};
-  return statement;
+  if (cid.every((x, k) => x[1] == after[k][1]))
+    return {status: "CLOSED", change: cid};
+  else
+    return {status: "OPEN", change: after.reverse().filter(x => x[1] > 0)};
 }
