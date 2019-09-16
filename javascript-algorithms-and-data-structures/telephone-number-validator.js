d6c555952e99ef0da7d84e035917e75feaa12c7e
Salah Adawi <53702890+Hahli144@users.noreply.github.com>
2019-09-16 13:29:42 +0300

    Update telephone-number-validator.js

--- javascript-algorithms-and-data-structures/telephone-number-validator.js
+++ javascript-algorithms-and-data-structures/telephone-number-validator.js
@@ -1,5 +1,5 @@
 function telephoneCheck(str) {
-  var regex = /^1?(\([0-9]{3}\)|[0-9]{3})-?[0-9]{3}-?[0-9]{4}$/;
+  var regex = /^1?(\([\d]{3}\)|[\d]{3})-?[\d]{3}-?[\d]{4}$/;
   if (str.replace(/ /g, "").search(regex) != -1)
     return true
   return false;
